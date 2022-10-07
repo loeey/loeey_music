d957fb78802acb45eac1d718e9d7e38c70adf85d
loeey <83129919+loeey@users.noreply.github.com>
2022-10-07 11:18:34 +0800

    Update request.js

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,5 @@
 // 网络请求
 // 封装好处 1.便于修改 2.便于更换,以后不用axios直接换,不会影响逻辑页面
 import axios from 'axios'
-axios.defaults.baseURL = "http://localhost:3000"
+axios.defaults.baseURL = "https://netease-cloud-music-api-lac-nine-54.vercel.app"
 export default axios
